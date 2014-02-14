9a4f066e3c65ebdb887b0d109d84b15bd9d13f4a
Hector Rivas <hector.rivas@springer.com>
2014-02-14 10:40:37 +0000

    Added $HOME/bin and $HOME/local/bin to $PATH

--- .my_bash/custom/20_extra_path.bash
+++ .my_bash/custom/20_extra_path.bash
@@ -1,2 +1,4 @@
 # Add MacOSX npm path if exists 
 [ -d /usr/local/share/npm/bin ] && PATH=$PATH:/usr/local/share/npm/bin
+[ -d $HOME/local/bin ] && PATH=$PATH:$HOME/local/bin
+[ -d $HOME/bin ] && PATH=$PATH:$HOME/bin
