38544422b6d62dc0b2c50e24e87887f4b102b702
Hector Rivas <hector.rivas@springer.com>
2014-02-14 18:09:35 +0000

    Updated the load of pass installations to make it more generic

--- .my_bash/custom/10_custom_pass.bash
+++ .my_bash/custom/10_custom_pass.bash
@@ -1,17 +1,4 @@
-# Create casper-pass alias to override the PASSWORD_STORE_DIR env variable
-alias casper-pass='PASSWORD_STORE_DIR=~/.casper-pass pass'
-alias pe-pass='PASSWORD_STORE_DIR=~/.pe-pass pass'
-
-# Load the bash completion definition
-[ -f /etc/bash_completion.d/password-store ] && source /etc/bash_completion.d/password-store
-[ -f /usr/local/etc/bash_completion.d/password-store ] && source /usr/local/etc/bash_completion.d/password-store
-[ -f ~/tools/password-store/contrib/pass.bash-completion ] && source ~/tools/password-store/contrib/pass.bash-completion
-
-# Small hack to allow use alternate path for PASSWORD_STORE_DIR in bash completion
-if type _pass > /dev/null 2>&1; then
-	_casper_pass_completion() { PASSWORD_STORE_DIR=$HOME/.casper-pass/ _pass; }
-	complete -o filenames -o nospace -F _casper_pass_completion casper-pass
-	_pe_pass_completion() { PASSWORD_STORE_DIR=$HOME/.pe-pass/ _pass; }
-	complete -o filenames -o nospace -F _pe_pass_completion pe-pass
-fi 
-
+# Load the custom .*-pass I have
+for i in ~/.*-pass; do 
+	[ -e $i/.load.bash ] && . $i/.load.bash
+done
