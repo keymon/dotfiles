04b031fcd02e36310076cac99073aaca22a94406
Hector Rivas Gandara <keymon@gmail.com>
2014-07-26 17:40:04 +0100

    Added code to manage boot2docker

--- .my_bash/custom/80_docker.bash
+++ .my_bash/custom/80_docker.bash
@@ -1,6 +1,7 @@
 # Support for docker-osx
-# Add $DOCKER_HOST if localdocker is in /etc/hosts
-grep -q localdocker /etc/hosts && export DOCKER_HOST="tcp://localdocker:4243"
-
-# Load bash completion if docker is in the path
-# which docker 2>&1 >/dev/null && . ~/.my_bash/completions/docker
+if which docker-osx &>/dev/null; then
+	# Add $DOCKER_HOST if localdocker is in /etc/hosts
+	grep -q localdocker /etc/hosts && export DOCKER_HOST="tcp://localdocker:4243"
+elif which boot2docker &>/dev/null; then
+	export DOCKER_HOST=$(boot2docker cfg 2> /dev/null | awk '/LowerIP/ { gsub(/"/, "", $3); ip=$3 } /DockerPort/ { port=$3 }  END { print "tcp://" ip ":" port }')
+fi
