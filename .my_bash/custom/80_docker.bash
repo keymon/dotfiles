ab41f1d726ce4e4ca8cc939d951a593a538561eb
Hector Rivas Gandara <keymon@gmail.com>
2014-10-19 22:01:19 +0100

    Initial vim config

--- .my_bash/custom/80_docker.bash
+++ .my_bash/custom/80_docker.bash
@@ -3,5 +3,6 @@ if which docker-osx &>/dev/null; then
 	# Add $DOCKER_HOST if localdocker is in /etc/hosts
 	grep -q localdocker /etc/hosts && export DOCKER_HOST="tcp://localdocker:4243"
 elif which boot2docker &>/dev/null; then
-	export DOCKER_HOST=$(boot2docker cfg 2> /dev/null | awk '/LowerIP/ { gsub(/"/, "", $3); ip=$3 } /DockerPort/ { port=$3 }  END { print "tcp://" ip ":" port }')
+	# export DOCKER_HOST=$(boot2docker cfg 2> /dev/null | awk '/LowerIP/ { gsub(/"/, "", $3); ip=$3 } /DockerPort/ { port=$3 }  END { print "tcp://" ip ":" port }')
+	export DOCKER_HOST=tcp://$(boot2docker ip 2> /dev/null):2375
 fi
---
+++ .vimrc
@@ -0,0 +1,6 @@
+" Include https://github.com/tpope/vim-pathogen
+execute pathogen#infect()
+
+" Syntax setup
+syntax on
+filetype plugin indent on
