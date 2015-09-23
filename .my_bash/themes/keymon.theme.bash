4aa6735c9f5373dc92f41059581be425610fbc02
Hector Rivas Gandara <keymon@gmail.com>
2015-09-23 23:40:25 +0100

    Revert "Make fix dirty line more resilient"
    This reverts commit 5caa528b1eabc76f46489541a761a2b1dfab9c3a.

--- .my_bash/themes/keymon.theme.bash
+++ .my_bash/themes/keymon.theme.bash
@@ -8,16 +8,19 @@ __my_rvm_ruby_version() {
 }
 
 # From answer in: http://stackoverflow.com/a/2575525/395686 by Dennis Williamson
-get_tty_cursor_col() {
+get_tty_cursor_position() {
     # based on a script from http://invisible-island.net/xterm/xterm.faq.html
     exec < /dev/tty
     local oldstty=$(stty -g)
     stty raw -echo min 0
+    # on my system, the following line can be replaced by the line below it
     echo -en "\033[6n" > /dev/tty
-    read -r -d R -a pos
+    # tput u7 > /dev/tty    # when TERM=xterm (and relatives)
+    IFS=';' read -r -d R -a pos
     stty $oldstty
     # change from one-based to zero based so they work with: tput cup $row $col
-    __cursor_col=${pos##*;} 2>&1 >/dev/null    # strip off the esc-[
+    __cursor_row=$((${pos[0]:2} - 1))    # strip off the esc-[
+    __cursor_col=$((${pos[1]} - 1))
 }
 
 prompt_setter() {
@@ -34,8 +37,8 @@ prompt_setter() {
   TITLEBAR="\033]0;${scm_info} \u@\H:\W\007"
 
   # Add a new line if the line in last command does not end in new line
-  get_tty_cursor_col
-  if [[ "$__cursor_col" -gt 1 ]]; then
+  get_tty_cursor_position
+  if [[ "$__cursor_col" -gt 0 ]]; then
         TITLEBAR="\n$TITLEBAR"
   fi
 
