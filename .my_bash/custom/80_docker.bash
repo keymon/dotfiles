976bb95bbd59efd8e5509dcf03240974920ec400
Hector Rivas Gandara <keymon@gmail.com>
2016-03-20 20:14:14 +0000

    Adapt docker init script to docker-machine

--- .my_bash/custom/80_docker.bash
+++ .my_bash/custom/80_docker.bash
@@ -1,15 +1,15 @@
-# Support for boot2docker
-boot2docker-shellinit() {
-	if which boot2docker &>/dev/null; then
-		$(boot2docker shellinit 2> /dev/null)
-		# In order to map the port, just use this. 
-		VBOX_MAP_PORT=$(VBoxManage showvminfo boot2docker-vm --machinereadable | sed -n 's/Forwarding.*docker,tcp,127.0.0.1,\(.*\),,2376.*/\1/p')
+# Support for docker-machine
+docker-machine-shellinit() {
+	if which docker-machine &>/dev/null; then
+		eval $(docker-machine env 2> /dev/null)
+		# In order to map the port, just use this.
+		VBOX_MAP_PORT=$(VBoxManage showvminfo $(docker-machine ls | grep virtualbox | awk '{print $1;}') --machinereadable 2> /dev/null | sed -n 's/Forwarding.*docker,tcp,127.0.0.1,\(.*\),,2376.*/\1/p')
 		if [ ! -z "$VBOX_MAP_PORT" ]; then
 			export DOCKER_HOST=tcp://127.0.0.1:$VBOX_MAP_PORT
 			echo "Docker port is mapped in Virtual Box. Using localhost: DOCKER_HOST=$DOCKER_HOST"
 		fi
-	else 
-		echo "Not able to find boot2docker in the PATH, skipping"
+	else
+		echo "Not able to find docker-machine in the PATH, skipping"
 	fi
 }
-boot2docker-shellinit > /dev/null
+docker-machine-shellinit > /dev/null
