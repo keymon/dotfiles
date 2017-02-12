df0e9653300871693cbea04ffa88fd1b6d245946
Hector Rivas Gandara <keymon@gmail.com>
2017-02-12 19:01:10 +0000

    Fix virtualenv quotes

--- .my_bash/custom/80_virtualenvwrapper.bash
+++ .my_bash/custom/80_virtualenvwrapper.bash
@@ -1,4 +1,4 @@
-VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
-if [ -f $(which virtualenvwrapper.sh 2> /dev/null) ]; then
+VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+if [ -f "$(which virtualenvwrapper.sh 2> /dev/null)" ]; then
 	source $(which virtualenvwrapper.sh)
 fi
