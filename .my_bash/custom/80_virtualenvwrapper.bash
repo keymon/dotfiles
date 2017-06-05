a0fc57a80e3d1abc652778b5fe1608bdf7cf13b4
Hector Rivas Gandara <keymon@gmail.com>
2017-06-05 11:15:47 +0100

    Be more smart looking for python
    just a little :)

--- .my_bash/custom/80_virtualenvwrapper.bash
+++ .my_bash/custom/80_virtualenvwrapper.bash
@@ -1,4 +1,9 @@
-VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+
+if [ -x "/usr/local/bin/python" ]; then
+	VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+else
+	VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
+fi
 if [ -f "$(which virtualenvwrapper.sh 2> /dev/null)" ]; then
 	source $(which virtualenvwrapper.sh)
 fi
