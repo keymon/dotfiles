2aac19d793c4604b4da3339e7a2bf2ea3a20a0f6
Hector Rivas Gandara <keymon@gmail.com>
2017-06-28 14:39:38 +0100

    Add mmgaws script to login in mmg AWS accounts

---
+++ .my_bash/custom/99_add_path.bash
@@ -0,0 +1,18 @@
+# via https://unix.stackexchange.com/a/4979/550
+#
+# define append_path and prepend_path to add directory paths, e.g. PATH, MANPATH
+# add to end of path
+append_path()
+{
+  if ! eval test -z "\"\${$1##*:$2:*}\"" -o -z "\"\${$1%%*:$2}\"" -o -z "\"\${$1##$2:*}\"" -o -z "\"\${$1##$2}\"" ; then
+    eval "$1=\$$1:$2"
+  fi
+}
+
+# add to front of path
+prepend_path()
+{
+  if ! eval test -z "\"\${$1##*:$2:*}\"" -o -z "\"\${$1%%*:$2}\"" -o -z "\"\${$1##$2:*}\"" -o -z "\"\${$1##$2}\"" ; then
+    eval "$1=$2:\$$1"
+  fi
+}
