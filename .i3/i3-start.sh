7ef2a8a3b67a2421c30fc01458adc6843834ea79
Benoth <benoth83@gmail.com>
2018-04-30 09:56:28 +0100

    Configure i3 start desktop.
    Includes a start wrapper script to run commands before i3,
    e.g. configure the hidpi

---
+++ .i3/i3-start.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -x
+exec > ~/.i3.log 2>&1
+
+# Settings for HiDPI
+gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "[{'Gdk/WindowScalingFactor', <2>}, {'Xft/DPI', <192000>} ]"
+xrandr --dpi 192
+
+# Keynoard rate
+xset r rate 250 40
+
+# Bell off
+xset -b
+
+# Add xrdb config
+xrdb -merge ~/.Xresources
+
+# Configure keyboard mapping
+xmodmap ~/.Xmodmap
+
+# Start i3
+i3
