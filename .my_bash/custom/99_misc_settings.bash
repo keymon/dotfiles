9dfdea5f9955be4201d1924a97dca133840c4929
Hector Rivas Gandara <keymon@gmail.com>
2020-09-04 18:20:56 +0100

    Add debian author env_vars

--- .my_bash/custom/99_misc_settings.bash
+++ .my_bash/custom/99_misc_settings.bash
@@ -2,3 +2,8 @@
 export EDITOR="vim"
 export GIT_EDITOR="vim"
 [ -n "$TMUX" -a "$TERM" == "screen" ] && export TERM="screen-256color"
+
+export DEBFULLNAME="Hector Rivas Gandara"
+export DEBEMAIL="keymon@gmail.com"
+export UBUMAIL="keymon@gmail.com"
+
