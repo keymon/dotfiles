18c93baa1b12b63b2299060d3b929124ec5b0bb9
Hector Rivas Gandara <keymon@gmail.com>
2020-09-16 11:36:57 +0100

    pything virtualenv support newer ubuntu distro

--- .my_bash/custom/80_virtualenvwrapper.bash
+++ .my_bash/custom/80_virtualenvwrapper.bash
@@ -2,8 +2,14 @@
 if [ -x "/usr/local/bin/python" ]; then
 	VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
 else
-	VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
+	VIRTUALENVWRAPPER_PYTHON="$(which python python2 python3  | head -n1)"
 fi
 if [ -f "$(which virtualenvwrapper.sh 2> /dev/null)" ]; then
 	source $(which virtualenvwrapper.sh)
+elif [ -f /usr/share/bash-completion/completions/virtualenvwrapper ]; then
+	source  /usr/share/bash-completion/completions/virtualenvwrapper
+fi
+
+if [[ -z "${VIRTUAL_ENV}" ]]; then
+	workon default 2>&1 > /dev/null || true
 fi
